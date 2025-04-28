6a018c431b196dcc369e7a12add4b39eae350f6a
coDEnAmeTurner <tranluuquoctuan191191@gmail.com>
2025-04-28 23:57:42 +0700

    #GameLoopManager #FrameListener

---
+++ The L-T-M Engine/The L-T-M Core/Engine Support System/Game Loop/GameLoopManager/GameLoopManager.cpp
@@ -0,0 +1,51 @@
+#include "GameLoopManager.h"
+
+std::unique_ptr<GameLoopManager> GameLoopManager::s_instance = nullptr;
+
+GameLoopManager::GameLoopManager() {
+	if (s_instance == nullptr) {
+		s_instance = std::make_unique<GameLoopManager>();
+	}
+}
+
+GameLoopManager::~GameLoopManager() {
+	s_instance = nullptr;
+}
+
+void GameLoopManager::init() {
+	GameLoopManager();
+}
+void GameLoopManager::destroy() {
+	
+}
+void GameLoopManager::pushFrameLis(std::shared_ptr<FrameListener> framelis) {
+	assert(framelis != nullptr || "Frame listner can not be null");
+
+	s_instance->m_frameLisList.push_back(framelis);
+}
+void GameLoopManager::eraseFrameLis(std::shared_ptr<FrameListener> framelis) {
+	assert(framelis != nullptr || "Frame listner can not be null");
+
+	auto it = std::find(
+		s_instance->m_frameLisList.begin(),
+		s_instance->m_frameLisList.end(),
+		framelis
+	);
+	assert(it != s_instance->m_frameLisList.end() || "Can not find frame listener");
+
+	s_instance->m_frameLisList.erase(it);
+}
+std::vector<std::shared_ptr<FrameListener>>& GameLoopManager::getFrameListnerList()
+{
+	return s_instance->m_frameLisList;
+}
+void GameLoopManager::renderMaster() {
+	//placeholder
+}
+
+std::unique_ptr<GameLoopManager>& GameLoopManager::getInstance() {
+	return s_instance;
+}
+bool GameLoopManager::getRunning() {
+	return s_instance->m_running;
+}
---
+++ The L-T-M Engine/The L-T-M Core/Engine Support System/Game Loop/Main.cpp
@@ -0,0 +1,37 @@
+#include <cstdint>
+#include <string>
+#include <cstring>
+#include <emmintrin.h>
+#include <intrin.h>
+#include <thread>
+#include <mutex>
+#include <windows.h>
+#include <DoubleEndedStackAllocator.h>
+#include <DoubleBufferedAllocator.h>
+#include <StackAllocator.h>
+#include <PoolAllocator.h>
+#include <FileHandler.h>
+#include <GameLoopManager.h>
+#include <Test.h>
+
+
+int main() {
+	GameLoopManager::init();
+
+	while (GameLoopManager::getRunning()) {
+		for (const auto& listener : GameLoopManager::getFrameListnerList()) {
+			listener->frameStarted();
+
+		}
+		
+		GameLoopManager::renderMaster();
+
+		for (const auto& listener : GameLoopManager::getFrameListnerList()) {
+			listener->frameEnded();
+
+		}
+	}
+
+	GameLoopManager::destroy();
+	return 0;
+}
